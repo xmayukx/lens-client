9ea52f920af9b5fa335aee8c75577eece604c9a0
Mayukh Hazari <hazari.mayukh77@gmail.com>
2023-11-03 01:07:47 +0530

    Add initial files

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,173 @@
-import Image from 'next/image'
+"use client";
+
+import { ChangeEvent } from "react";
+import toast from "react-hot-toast";
 
 export default function Home() {
+  const handlelogin = async () => {
+    const handleInput = document.querySelector(
+      'input[name="handle"]'
+    ) as HTMLInputElement;
+    const pvtKeyInput = document.querySelector(
+      'input[name="pvtKey"]'
+    ) as HTMLInputElement;
+    console.log("Handle:", handleInput?.value);
+    console.log("Private Key:", pvtKeyInput?.value);
+
+    const res = await fetch("http://localhost:8000/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        handle: handleInput?.value,
+        pvtKey: pvtKeyInput?.value,
+      }),
+    });
+    const data = await res.json();
+    toast("Logged In", data);
+  };
+
+  const handledispatch = async () => {
+    const handleInput = document.querySelector(
+      'input[name="handle"]'
+    ) as HTMLInputElement;
+    console.log("Handle:", handleInput?.value);
+    const body = JSON.stringify({
+      handle: handleInput?.value,
+    });
+    const res = await fetch("http://localhost:8000/enable-dispatcher", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: body,
+    });
+    const data = await res.json();
+    if (data.status === "401") {
+      toast.error("Unauthorized", data);
+      return;
+    }
+    toast.success("Dispatcher Enabled", data);
+  };
+
+  const handleprofile = async () => {
+    const handleInput = document.querySelector(
+      'input[name="handle"]'
+    ) as HTMLInputElement;
+    const evmAddressInput = document.querySelector(
+      'input[name="evmAddress"]'
+    ) as HTMLInputElement;
+    const pvtKeyInput = document.querySelector(
+      'input[name="pvtKey"]'
+    ) as HTMLInputElement;
+
+    console.log("Handle:", handleInput.value);
+    console.log("EVM Address:", evmAddressInput.value);
+    console.log("Private Key:", pvtKeyInput.value);
+    const body = JSON.stringify({
+      handle: handleInput?.value,
+      pvtKey: pvtKeyInput?.value,
+      evmAddress: evmAddressInput?.value,
+    });
+    // JSON.stringify({
+    //   handle: handleInput?.value,
+    //   pvtKey: pvtKeyInput?.value,
+    //   evmAddress: evmAddressInput?.value,
+    // })
+    const res = await fetch("http://localhost:8000/create-profile", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: body,
+    });
+    const data = await res.json();
+    toast("Profile Created", data);
+  };
+
+  const handlepost = async () => {
+    const handleInput = document.querySelector(
+      'input[name="text"]'
+    ) as HTMLInputElement;
+
+    console.log("Handle:", handleInput.value);
+    const body = JSON.stringify({
+      text: handleInput?.value,
+    });
+    const res = await fetch("http://localhost:8000/create-post", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: body,
+    });
+    const data = await res.json();
+    toast("Post Created", data);
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <main className=" text-black">
+      <div className="flex flex-col mx-52">
+        <h1 className="text-3xl font-bold items-center justify-center flex">
+          Create Profile
+        </h1>
+        <form className=" flex flex-col space-y-3 mb-2">
+          <input type="text" name="handle" placeholder="handle" />
+          <input type="text" name="evmAddress" placeholder="evmAddress" />
+          <input type="text" name="pvtKey" placeholder="pvtKey" />
+        </form>
+        <button
+          onClick={handleprofile}
+          className=" bg-white px-2 py-1 text-black relative"
+        >
+          create-profile
+        </button>
+      </div>
+      <div className="flex flex-col mx-52">
+        <h1 className="text-3xl font-bold items-center justify-center flex">
+          login
+        </h1>
+        <form action="post" className=" flex flex-col space-y-3 mb-2">
+          <input type="text" name="handle" placeholder="handle" />
+          <input type="text" name="pvtKey" placeholder="pvtKey" />
+        </form>
+        <button
+          onClick={handlelogin}
+          className=" bg-white px-2 py-1 text-black relative"
+        >
+          login
+        </button>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="flex flex-col mx-52">
+        <h1 className="text-3xl font-bold items-center justify-center flex">
+          Enable Dispatcher
+        </h1>
+        <form action="post" className=" flex flex-col space-y-3 mb-2">
+          <input type="text" name="handle" placeholder="handle" />
+        </form>
+        <button
+          onClick={handledispatch}
+          type="submit"
+          className=" bg-white px-2 py-1 text-black relative"
+        >
+          Enable Dispatcher
+        </button>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="flex flex-col mx-52">
+        <h1 className="text-3xl font-bold items-center justify-center flex">
+          Post on LP
+        </h1>
+        <form action="post" className=" flex flex-col space-y-3 mb-2">
+          <input type="text" name="text" placeholder="content URI" />
+        </form>
+        <button
+          onClick={handlepost}
+          className=" bg-white px-2 py-1 text-black relative"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          post
+        </button>
       </div>
     </main>
-  )
+  );
 }
